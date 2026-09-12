5751bbcfd1393f8e0e51a8e4a61156945014b907
nacosking <teohyihern28@gmail.com>
2026-09-13 05:46:35 +0800

    updated on product list (added infinite scroll for products)

--- app/src/main/java/com/example/product_catalog/MainPage.kt
+++ app/src/main/java/com/example/product_catalog/MainPage.kt
@@ -1,7 +1,9 @@
 package com.example.product_catalog
 
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,15 +18,22 @@ import kotlinx.coroutines.launch
 class MainPage : AppCompatActivity() {
 
     private lateinit var productRecyclerView: RecyclerView
+    private lateinit var loadingProgressBar: ProgressBar
     private lateinit var searchBox: EditText
     private lateinit var adapter: ProductListAdapter
     private var respond = APIRespond()
+
+    private var currentSkip = 0
+    private val pageLimit = 20
+    private var isLoading = false
+    private var hasMoreProducts = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
 
         productRecyclerView = findViewById(R.id.productRecyclerView)
+        loadingProgressBar = findViewById(R.id.loadingProgressBar)
 
         RecyclerView()
         loadProductsFromAPI()
@@ -38,21 +47,58 @@ class MainPage : AppCompatActivity() {
         productRecyclerView.apply {
             layoutManager = GridLayoutManager (this@MainPage, 1)
             adapter = this@MainPage.adapter
+
+            addOnScrollListener(PaginationScroll())
         }
     }
 
     private fun loadProductsFromAPI() {
+        if (isLoading || !hasMoreProducts) return
+
+        isLoading = true
+        loadingProgressBar.visibility = View.VISIBLE
+
         CoroutineScope(Dispatchers.Main).launch {
-            val result = respond.getProducts(limit = 20, skip = 0)
+            val result = respond.getProducts(limit = pageLimit, skip = currentSkip)
 
             result.onSuccess { response ->
-                adapter.submitList(response.products)
+                if (currentSkip == 0 ) {
+                    adapter.submitList(response.products)
+                } else {
+                    adapter.addMoreProducts(response.products)
+                }
+
+                currentSkip += pageLimit
+                hasMoreProducts = response.products.isNotEmpty()
+                isLoading = false
+                loadingProgressBar.visibility = View.GONE
             }
 
             result.onFailure { error ->
-
+                isLoading = false
+                loadingProgressBar.visibility = View.GONE
+                println("Error: ${error.message}")
             }
 
+        }
+    }
+
+    private inner class PaginationScroll : RecyclerView.OnScrollListener(){
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int){
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as GridLayoutManager
+            val visibileItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+            val firstVisibleItemPositions = layoutManager.findFirstVisibleItemPosition()
+
+            if (visibileItemCount + firstVisibleItemPositions >= totalItemCount
+                && firstVisibleItemPositions >= 0
+                && !isLoading) {
+
+                loadProductsFromAPI()
+
+            }
         }
     }
 }
